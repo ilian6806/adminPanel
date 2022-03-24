0eafb94e961468097451db47f3f4a67d7a332c1f
ilian.iliev <ilian.iliev@imperiaonline.org>
2022-03-24 16:38:47 +0200

    Badge component implemented

--- admin-panel/src/pages/dashboard/dashboard.component.jsx
+++ admin-panel/src/pages/dashboard/dashboard.component.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React from "react";
 import CustomDataTable from "../../components/custom-data-table/custom-data-table.component";
+import Badge from "../../components/badge/badge.component";
 import testPaymentsData from "../../test-data/payments"
 import testPaymentsMonthData from "../../test-data/payments-month"
 
@@ -8,6 +9,11 @@ const Dashboard = () => {
         <div>
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
+            </div>
+            <div className="row">
+                <Badge title="EARNINGS (MONTHLY)" value="$40,000" />
+                <Badge title="EARNINGS (ANNUAL)" value="$215,000" color="success" icon="calendar" />
+                <Badge title="RETENTION (FIRST DAY)" progress="22" color="info" icon="comments" />
             </div>
             <CustomDataTable
                 title="Per Month"
