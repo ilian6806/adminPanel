8371208277bcf6f9bc0f1590c1c09911bfd3399a
ilian.iliev <ilian.iliev@imperiaonline.org>
2022-04-04 18:40:06 +0300

    Test CI

--- admin-panel/src/App.test.js
+++ admin-panel/src/App.test.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { act } from 'react-dom/test-utils';
 import App from "./App";
 
 it('Renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
-    throw 'test'
 });
